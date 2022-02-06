8ae578176ebb459fdba6b289660f64df8afc2be0
Steren <steren@google.com>
2022-02-06 21:19:19 +0000

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
 # User Journeys demo
 
-Build:
+This demo shows how to replay recorded user journeys of your website on Cloud Run jobs.
+
+## Record your user journeys
+
+1. Use [Chrome DevTools' Recorder](https://developer.chrome.com/docs/devtools/recorder/) to record critical user journeys for your publicly accessible website.
+1. Export the replay using DevTools' Recorder [export feature](https://developer.chrome.com/docs/devtools/recorder/#edit-flows)
+1. Save the exported `.js` file under the `journeys/` folder.
+
+## Deploying to Google Cloud Platform
+
+1. Install the [`gcloud` command line](https://cloud.google.com/sdk/docs/install).
+1. Create a Google Cloud project.
+1. Configure your local `gcloud` to use your project:
+   ```
+   gcloud config set project YOUR_PROJECT_ID
+   ```
+1. Configure a region to use for Cloud Run, replace `us-central1` with any [available regions](https://cloud.google.com/run/docs/locations):
+   ```
+   gcloud config set run/region us-central1
+   ```
+1. Create a new Artifact Registry container repository:
+   ```
+   gcloud artifacts repositories create containers --repository-format=docker --location=us-central1
+   ```
+1. Build this repository into a container image:
+  ```
+  gcloud builds submit -t us-central1-docker.pkg.dev/YOUR_PROJECT_ID/containers/user-journeys-demo
+  ```
+1. Create a Cloud Run job, replace `your-site.com/user-journey.js` with the relative link to your user journey `.js` file:
+  ```
+  gcloud alpha run jobs create user-journeys-demo --image us-central1-docker.pkg.dev/steren-serverless/containers/user-journeys-demo:latest --args your-site.com/user-journey.js
+  ```
+1. Run the Cloud Run job:
+  ```
+  gcloud alpha run jobs run user-journeys-demo
+  ```
+
+
+
+
+## Testing locally
+
+The following steps assume you have `docker` installed on your local machine. If you don't proceed to the next section to deploy to Google Cloud.
+
+Build with:
 
 ```
 docker build . -t user-journeys-demo
@@ -9,7 +53,6 @@ docker build . -t user-journeys-demo
 Run locally:
 
 ```
-docker run --cap-add=SYS_ADMIN user-journeys-demo journeys/cloud.google.com/home-to-cloud-run.js
+docker run --cap-add=SYS_ADMIN user-journeys-demo example.com/more-info.js
 ```
 
-Replace `journeys/cloud.google.com/home-to-cloud-run.js` with the any export of Chrome DevTools' Record 
---
+++ journeys/example.com/more-info.js
@@ -0,0 +1,140 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    async function waitForSelectors(selectors, frame) {
+      for (const selector of selectors) {
+        try {
+          return await waitForSelector(selector, frame);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      throw new Error('Could not find element for selectors: ' + JSON.stringify(selectors));
+    }
+
+    async function waitForSelector(selector, frame) {
+      if (selector instanceof Array) {
+        let element = null;
+        for (const part of selector) {
+          if (!element) {
+            element = await frame.waitForSelector(part);
+          } else {
+            element = await element.$(part);
+          }
+          if (!element) {
+            throw new Error('Could not find element: ' + part);
+          }
+          element = (await element.evaluateHandle(el => el.shadowRoot ? el.shadowRoot : el)).asElement();
+        }
+        if (!element) {
+          throw new Error('Could not find element: ' + selector.join('|'));
+        }
+        return element;
+      }
+      const element = await frame.waitForSelector(selector);
+      if (!element) {
+        throw new Error('Could not find element: ' + selector);
+      }
+      return element;
+    }
+
+    async function waitForElement(step, frame) {
+      const count = step.count || 1;
+      const operator = step.operator || '>=';
+      const comp = {
+        '==': (a, b) => a === b,
+        '>=': (a, b) => a >= b,
+        '<=': (a, b) => a <= b,
+      };
+      const compFn = comp[operator];
+      await waitForFunction(async () => {
+        const elements = await querySelectorsAll(step.selectors, frame);
+        return compFn(elements.length, count);
+      });
+    }
+
+    async function querySelectorsAll(selectors, frame) {
+      for (const selector of selectors) {
+        const result = await querySelectorAll(selector, frame);
+        if (result.length) {
+          return result;
+        }
+      }
+      return [];
+    }
+
+    async function querySelectorAll(selector, frame) {
+      if (selector instanceof Array) {
+        let elements = [];
+        let i = 0;
+        for (const part of selector) {
+          if (i === 0) {
+            elements = await frame.$$(part);
+          } else {
+            const tmpElements = elements;
+            elements = [];
+            for (const el of tmpElements) {
+              elements.push(...(await el.$$(part)));
+            }
+          }
+          if (elements.length === 0) {
+            return [];
+          }
+          const tmpElements = [];
+          for (const el of elements) {
+            const newEl = (await el.evaluateHandle(el => el.shadowRoot ? el.shadowRoot : el)).asElement();
+            if (newEl) {
+              tmpElements.push(newEl);
+            }
+          }
+          elements = tmpElements;
+          i++;
+        }
+        return elements;
+      }
+      const element = await frame.$$(selector);
+      if (!element) {
+        throw new Error('Could not find element: ' + selector);
+      }
+      return element;
+    }
+
+    async function waitForFunction(fn) {
+      let isActive = true;
+      setTimeout(() => {
+        isActive = false;
+      }, 5000);
+      while (isActive) {
+        const result = await fn();
+        if (result) {
+          return;
+        }
+        await new Promise(resolve => setTimeout(resolve, 100));
+      }
+      throw new Error('Timed out');
+    }
+    {
+        const targetPage = page;
+        await targetPage.setViewport({"width":1689,"height":1190})
+    }
+    {
+        const targetPage = page;
+        const promises = [];
+        promises.push(targetPage.waitForNavigation());
+        await targetPage.goto('https://example.com/');
+        await Promise.all(promises);
+    }
+    {
+        const targetPage = page;
+        const promises = [];
+        promises.push(targetPage.waitForNavigation());
+        const element = await waitForSelectors([["aria/More information..."],["body > div > p:nth-child(3) > a"]], targetPage);
+        await element.click({ offset: { x: 66.45458984375, y: 4.6022796630859375} });
+        await Promise.all(promises);
+    }
+
+    await browser.close();
+})();
